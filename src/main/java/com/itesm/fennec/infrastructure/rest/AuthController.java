972014cb7022315ca3f0545f7ac07b81c95e22be
diego <diego.roque0309@gmail.com>
2025-05-20 20:29:32 -0600

    TestController

--- src/main/java/com/itesm/fennec/infrastructure/rest/AuthController.java
+++ src/main/java/com/itesm/fennec/infrastructure/rest/AuthController.java
@@ -1,14 +1,22 @@
 package com.itesm.fennec.infrastructure.rest;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
 import com.itesm.fennec.application.useCase.auth.SignUpUseCase;
+import com.itesm.fennec.domain.model.User;
+import com.itesm.fennec.domain.repository.UserRepository;
 import com.itesm.fennec.infrastructure.dto.auth.SignUpDTO;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.Response;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Path("/auth")
 public class AuthController {
@@ -29,6 +37,39 @@ public class AuthController {
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity(error)
                     .build();
+        }
+    }
+    @Inject
+    UserRepository userRepository;
+
+    @POST
+    @Path("/google")
+    public Response registerWithGoogle(@Context HttpHeaders headers) {
+        String idToken = headers.getHeaderString(HttpHeaders.AUTHORIZATION);
+        if (idToken == null || !idToken.startsWith("Bearer ")) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+
+        idToken = idToken.substring(7); // remove "Bearer "
+
+        try {
+            FirebaseToken firebaseToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+            String uid = firebaseToken.getUid();
+            String email = firebaseToken.getEmail();
+            String name = firebaseToken.getName();
+
+
+            User newUser = new User();
+            newUser.setFirebaseId(uid);
+            newUser.setEmail(email);
+            newUser.setNombre(name);
+            userRepository.createUser(newUser);
+
+
+            return Response.ok().build();
+        } catch (FirebaseAuthException e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid token").build();
         }
     }
 }
