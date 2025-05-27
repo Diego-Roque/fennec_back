8ce5690a7c71dea0f799a10e6560e0e490cd1123
Tavera <andrestavera03@gmail.com>
2025-05-26 23:36:01 -0600

    Investment update

--- src/main/java/com/itesm/fennec/domain/repository/PropertyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.itesm.fennec.domain.repository;
-
-import com.itesm.fennec.domain.model.Property;
-import com.itesm.fennec.domain.model.PaginatedResult;
-
-public interface PropertyRepository {
-    PaginatedResult<Property> getAllProperties(int page, int limit);
-}
