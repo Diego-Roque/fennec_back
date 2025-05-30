fee5625b15ad6b0a595148011b22e36cab8ba886
diego <diego.roque0309@gmail.com>
2025-05-29 19:30:46 -0600

    Reportes fixed

--- src/main/java/com/itesm/fennec/application/useCase/InsertarInformacionValuacionUseCase.java
+++ src/main/java/com/itesm/fennec/application/useCase/InsertarInformacionValuacionUseCase.java
@@ -2,8 +2,10 @@ package com.itesm.fennec.application.useCase;
 
 import com.itesm.fennec.application.service.InformeValuacionService;
 import com.itesm.fennec.domain.model.InformeValuacion;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+@ApplicationScoped
 public class InsertarInformacionValuacionUseCase {
     @Inject
     InformeValuacionService informeValuacionService;
